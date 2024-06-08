3023edff2424bc8f69b2e9ee49c9dc1648bbb447
NasaReabroi <700238647@fvtc.edu>
2024-06-07 22:04:16 -0500

    home page

---
+++ index.php
@@ -0,0 +1,34 @@
+!<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link type="text/css" rel="stylesheet" href="css/base.css">
+</head>
+<body>
+<header>
+    <h1>PHP Class - Reabroi's Website</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="/">Homepage</a></li>
+        <li><a href="#">Loop</a> </li>
+        <li><a href="#">Countdown</a> </li>
+        <li><a href="#">Other</a> </li>
+
+    </ul>
+</nav>
+<main>
+    <img src="img/me.PNG" alt="picture of myself">
+    <p>2 years of learning about coding and always can't remember the code I wrote it down.</p>
+    <p>I'll try my best to not make you disappointing in me and my coding skills.</p>
+
+</main>
+<footer>
+    <h6>&copy; 2024 Reabroi.com</h6>
+</footer>
+</body>
+</html>
