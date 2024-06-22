e3094a724d674f470ac9e3510812fd70eef35e42
NasaReabroi <700238647@fvtc.edu>
2024-06-22 02:21:39 -0500

    home page

--- Magic8/Magic8.php
+++ Magic8/Magic8.php
@@ -1,53 +1,39 @@
 <?php
 session_start();
 
+$question = isset($_POST["txtQuestion"]) ? $_POST["txtQuestion"] : "";
+$PrevQuest = isset($_SESSION["PrevQuest"]) ? $_SESSION["PrevQuest"] : "";
 
+$responses = array(
+    "Ask again later",
+    "Yes",
+    "No",
+    "It appears to be so",
+    "Reply is hazy, please try again",
+    "Yes, definitely",
+    "What is it you really want to know?",
+    "Outlook is good",
+    "My sources say no",
+    "Signs point to yes",
+    "Don't count on it",
+    "Cannot predict now",
+    "As I see it, yes",
+    "Better not tell you now",
+    "Concentrate and ask again"
+);
 
-if(isset($_POST["txtQuestion"])) {
-    $question = $_POST["txtQuestion"];
-}else{
-    $question ="";
-}
-
-if(isset($_SESSION["PrevQuest"])){
-    $PrevQuest = $_SESSION["PrevQuest"];
-}else{
-    $PrevQuest ="";
-}
-
-
-$responses = array();
-$responses [0] = "Ask again later";
-$responses [1] = "Yes";
-$responses [2] = "No";
-$responses [3] = "It appears ti be so";
-$responses [4] = "Reply is hazy , please try again";
-$responses [5] = "Yes, definitely";
-$responses [6] = "What is it you really want to know ?";
-$responses [7] = "Outlook is good";
-$responses [8] = "My sources say no";
-$responses [9] = "Signs point to yes";
-$responses [10] = "Don't count on it";
-$responses [11] = "Cannot predict now";
-$responses [12] = "As I see it, Yes";
-$responses [13] = "Better not tell you now";
-$responses [14] = "Concentrate and ask again";
-
-if($question=="") {
-    $answer ="Ask me a Question";
-}elseif(substr($question,-1) !="?") {
-    $answer ="Ask me with a Question Mark????";
-}elseif($PrevQuest==$question){
-    $answer ="Please ask a New Question!!!!";
-}else{
-    $iResponse = mt_rand(0,14);
-    $answer =$responses[$iResponse];
+if ($question == "") {
+    $answer = "Ask me a Question";
+} elseif (substr($question, -1) != "?") {
+    $answer = "Ask me with a Question Mark????";
+} elseif ($PrevQuest == $question) {
+    $answer = "Please ask a New Question!!!!";
+} else {
+    $iResponse = mt_rand(0, 14);
+    $answer = $responses[$iResponse];
     $_SESSION["PrevQuest"] = $question;
 }
-
 ?>
-
-
 
 <!doctype html>
 <html lang="en">
@@ -56,28 +42,32 @@ if($question=="") {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Magic8 Ball</title>
     <link type="text/css" rel="stylesheet" href="../css/base.css">
 </head>
-
 <body>
-<header><?php include '../includes/header.php'?></header>
-<nav><?php include '../includes/nav.php'?></nav>
+<header><?php include '../includes/header.php'; ?></header>
+<nav><?php include '../includes/nav.php'; ?></nav>
 
 <main>
-    <h2>Magic8 ball</h2>
+    <h2>Magic 8 Ball</h2>
     <br />
     <marquee>Ask me a question</marquee>
     <br />
-    <p>Ask a Question:<br />
+    <p>Ask a Question:</p>
     <form method="post" action="Magic8.php">
-    <input type="text" name="txtQuestion" id="txtQuestion"></p>
+        <input type="text" name="txtQuestion" id="txtQuestion">
         <input type="submit" value="Ask the 8 Ball">
     </form>
+
+    <?php if (!empty($question)): ?>
+        <p><strong>Your Question:</strong> <?php echo htmlspecialchars($question); ?></p>
+        <p><strong>Answer:</strong> <?php echo htmlspecialchars($answer); ?></p>
+    <?php endif; ?>
 </main>
 
 <footer>
-    <?php include '../includes/footer.php'?>
+    <?php include '../includes/footer.php'; ?>
 </footer>
 
 </body>
