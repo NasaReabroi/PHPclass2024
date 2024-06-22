a37504c0709d0440d6c81162e63e2e130d797198
NasaReabroi <700238647@fvtc.edu>
2024-06-22 02:00:43 -0500

    home page

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,6 @@
     <li><a href="/">Homepage</a></li>
     <li><a href="/loops/loops.php">Loop</a> </li>
     <li><a href="/Clock/countdown.php">Countdown</a> </li>
-    <li><a href="/Magic8/Magic8.php">Magic 9 ball</a> </li>
+    <li><a href="/Magic8/Magic8.php">Magic 8 ball</a> </li>
     <li><a href="/DiceRoller/play.php">DiceRoller</a> </li>
 </ul>
